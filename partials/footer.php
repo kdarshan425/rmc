4a869a5b8b620262c07b42f1b5ab42a29bffd8e2
kdarshan425 <64018424+kdarshan425@users.noreply.github.com>
2021-08-27 15:11:47 +0530

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,59 @@
 <div style="padding-top:0px;">
 <footer style="background: black;padding-top: 24px;padding-bottom: 24px; width: 100%">
+    <div class="container">
+        <div class="row" style="padding-bottom:20px;padding-top:50px;border-bottom:1px #c7c5c5 solid;">
+            <div class="col-lg-4">
+                <div class="container">
+                <p style="font-size:13px;color: #c7c5c5;font-weight: 300;font-family: 'Raleway', sans-serif;">
+                        The biggest advantage of Ready Mix Concrete (RMC) is that this concrete
+                    is specifically batched for your construction project at our batching plant and delivered directly at 
+                    your work site. We at Sai RMC India, use the latest and most advanced equipment to batch RMC under controlled
+                    conditions to ensure that the mixture is precision-specific for the kind of building or intended application.
+                    Depending on your specific need and the distance of your work site from our batching plant, our team will use the
+                    right formula and admixtures and retarders to ensure proper hydration.
+                    
+                </p>
+                </div>            
+            </div>
+            <div class="col-lg-4">
+                <center>
+                <div style="padding:10px;">
+                    <center> <p style="color: #c7c5c5;">Quick Links</p></center>
+                </div>
+                <p style="color: #c7c5c5;font-weight: 300;font-family: 'Raleway', sans-serif;">
+                    Home <br>
+                    About Us <br>
+                    Products <br>
+                    Gallery <br>
+                    Contact Us <br>
+                </p>
+                </center>
+                
+            </div>
+            <div class="col-lg-4">
+                <center>
+                <div style="padding:10px;">
+                <center><p style="color: #c7c5c5;">Info</p></center>                    
+                </div>
+                <p style="color: #c7c5c5;font-weight: 300;font-family: 'Raleway', sans-serif;">
+                 
+                    Tel: +91-9890929009, +91-9730009009 <br>
+
+                    Mob: +91-9881940094, <br>
+
+                    Email: info@sairmcindia.com <br>
+
+                    Working Hours: Mon - Sat 9:00 - 19:00 <br>
+                </p>
+                </center>
+                
+            </div>
+        </div>
+    </div>        
     <center>
-        <p style="color: white;font-weight: 300;font-family: 'Raleway', sans-serif;">All rights reserved <br> © SAI RMC INDIA</p>
+        <div style="padding-top:20px;">
+            <p style="color: #c7c5c5;font-weight: 300;font-family: 'Raleway', sans-serif;">All rights reserved <br> © SAI RMC INDIA</p>
+        </div>
     </center>            
 </footer>
 </div>
